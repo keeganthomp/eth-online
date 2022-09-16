9cd79b9c90fb1e70c959711f3dc20e79bd3c6add
Keegan Thompson <whereiskeegan@gmail.com>
2022-09-15 17:58:45 -0700

    layout stuff

--- client/src/components/sidebar/LeftSidebar.tsx
+++ client/src/components/sidebar/LeftSidebar.tsx
@@ -8,13 +8,26 @@ const LogoContainer = styled.div`
   height: 5rem;
   width: 100%;
 `;
+const PurchaseTokensContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-gap: 1rem;
+`;
+const PurchaseButton = styled.button`
+  padding: 0.5rem 0;
+  cursor: pointer;
+`;
 
 const LeftSidebar = () => {
   return (
     <Sidebar gridArea='leftSidebar'>
       <LogoContainer>
-        <p>Logo</p>
+        <p>Ixnay</p>
       </LogoContainer>
+      <PurchaseTokensContainer>
+        <PurchaseButton>Purchase Invite Token</PurchaseButton>
+        <PurchaseButton>Purchase Like Tokens</PurchaseButton>
+      </PurchaseTokensContainer>
     </Sidebar>
   );
 };
--- client/src/components/sidebar/RightSidebar.tsx
+++ client/src/components/sidebar/RightSidebar.tsx
@@ -3,19 +3,35 @@ import styled from 'styled-components';
 import CreatePost from 'components/Post/CreatePost';
 import Sidebar from './sidebar.component';
 
-const ButtonContainer = styled.div`
+const WalletContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  flex-direction: column;
   height: 5rem;
+`;
+const BalanceContainer = styled.div`
+  margin-top: 0.25rem;
+`;
+const Balance = styled.p`
+  font-size: 14px;
+  font-style: italic;
+`;
+const BalAmount = styled.span`
+  font-weight: bold;
 `;
 
 const RightSidebar = () => {
   return (
     <Sidebar gridArea='rightSidebar'>
-      <ButtonContainer>
+      <WalletContainer>
         <ConnectButton />
-      </ButtonContainer>
+        <BalanceContainer>
+          <Balance>
+            Like tokens: <BalAmount>34</BalAmount>
+          </Balance>
+        </BalanceContainer>
+      </WalletContainer>
       <CreatePost />
     </Sidebar>
   );
